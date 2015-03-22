3cc43792534525c98b2e4cb9c603b131a3a46a57
Kat Betz <katbetz@ufl.edu>
2015-03-21 23:56:36 -0400

    added wallpaper js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
 
+$(".walled").wallpaper({
+	source: {
+		"fallback": "img/header_bg.jpg",
+		"(min-width: 980px)": "img/header_bg.jpg"
+	}
+});
+
+$("main").wallpaper({
+	source: {
+		"fallback": "img/main_bg.png",
+		"(min-width: 980px)": "img/main_bg.png"
+	}
+});
+
 
 $('.tooltip').tooltipster({
     trigger: 'click'
